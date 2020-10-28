88c0392def595f4ca8d222d04feb513f16d49537
Diego Alejos <dalejos@rategenius.com>
2020-10-28 12:05:29 -0500

    more compatibility changes

--- gcpjwt/jwt_signer.py
+++ gcpjwt/jwt_signer.py
@@ -58,15 +58,16 @@ class JWTSigner:
 
     def sign(self, ring: str, key: str, data: bytes, hash: str = 'sha256'):
         key_path = self.client.crypto_key_path(self.project, self.location, ring, key)
-        enabled_keys = [e for e in list(self.client.list_crypto_key_versions(key_path))
+        enabled_keys = [e for e in list(self.client.list_crypto_key_versions(parent=key_path))
                         if e.state == kms_enums.CryptoKeyVersion.CryptoKeyVersionState.ENABLED]
         latest = sorted(enabled_keys, key=lambda e: e.create_time.seconds, reverse=True)[0]
         type = kms_enums.CryptoKeyVersion.CryptoKeyVersionAlgorithm(latest.algorithm).name.split('_')[0]
-        return type, self.client.asymmetric_sign(latest.name, {hash: self._hash_map[hash](data)}).signature
+        return type, self.client.asymmetric_sign(name=latest.name, digest=self._hash_map[hash](data)).signature
 
-    def verify(self, ring: str, key: str, payload: bytes, signature: bytes, force_latest: bool = False, hash: str = 'sha256'):
+    def verify(self, ring: str, key: str, payload: bytes, signature: bytes, force_latest: bool = False,
+               hash: str = 'sha256'):
         key_path = self.client.crypto_key_path(self.project, self.location, ring, key)
-        enabled_keys = [e for e in list(self.client.list_crypto_key_versions(key_path))
+        enabled_keys = [e for e in list(self.client.list_crypto_key_versions(parent=key_path))
                         if e.state == kms_enums.CryptoKeyVersion.CryptoKeyVersionState.ENABLED]
         if len(enabled_keys) == 0:
             return None, False
@@ -76,7 +77,8 @@ class JWTSigner:
             return self._verify_task(latest, payload, signature, hash)
         else:
             if self._thread_pool is not None:
-                futures = [self._thread_pool.submit(self._verify_task, k, payload, signature, hash) for k in enabled_keys]
+                futures = [self._thread_pool.submit(self._verify_task, k, payload, signature, hash) for k in
+                           enabled_keys]
                 concurrent.futures.wait(futures)
                 concurrent_result = map(lambda x: x.result(), futures)
                 if any([r[1] for r in concurrent_result]):
